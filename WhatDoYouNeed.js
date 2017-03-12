1fb65b79abb60d42101389bd5799ae065bfd98ce
ogundujo24 <ogundujoshua@gmail.com>
2017-03-11 21:13:00 -0500

    Create WhatDoYouNeed.js

---
+++ WhatDoYouNeed.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("button").click(function(event) {
+		/* Act on the event */
+		alert("Request has been sent! We look forward to working with you all to better serve the homeless community!");
+	});
+	
+});
